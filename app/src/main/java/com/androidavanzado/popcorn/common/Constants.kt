d79e9db992fadcd57d6d1a27d46f69b87cd12ad3
David Pineda <davidpineda1003@gmail.com>
2020-05-23 15:12:47 -0500

    Creando los Modelos

---
+++ app/src/main/java/com/androidavanzado/popcorn/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.androidavanzado.popcorn.common
+
+class Constants {
+    companion object {
+        public val TMDBAPI_BASE_URL = "https://api.themoviedb.org/3/"
+        public val API_KEY = "6d100791dc51e9be9b074af8c9ebe313"
+        public val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+    }
+}
